7921229ccede6aea213d1e3ff1fb7bc94af8f621
ARAALKAN1 <55189109+ARAALKAN1@users.noreply.github.com>
2019-09-13 12:14:39 +0200

    Sum 2 int

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,15 @@
 /* Here, you should entry the required libraries */
 
 void main(){
-	/* Here, you should write the source code of your program */
+
+	int firstNumber, secondNumber, sum;
+	
+	printf("Enter two integers: ");
+	
+	scanf("%d %d", &firstNumber, &secondNumber);
+	
+	sum=firstNumber + secondNumber;
+	printf("%d + %d = %d",	firstNumber, secondNumber, sum);
+	
+	return 0;/* Here, you should write the source code of your program */
 }
